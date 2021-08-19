e2cd640ad5c045128648649a96383fd4fa4133fa
Andrii Andrunevchyn <andrii.andrunevchyn@gmail.com>
2021-08-19 14:02:53 +0300

    Add project files.

---
+++ Data.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace RestAPIGetter
+{
+    public class Data
+    {
+        public IList<Products> Products { get; set; }
+        public IList<Categories> Categories { get; set; }
+    }
+
+    public class Products
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+    }
+
+    public class Categories
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using ConsoleTables;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace RestAPIGetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetUserInput();
+        }
+
+        static void GetUserInput()
+        {
+            Console.WriteLine("Enter command(-help for more information):");
+            string userInput = Console.ReadLine();
+            while (userInput != "-exit")
+            {
+                if (userInput == "-help")
+                    WriteHelpInfoInConsole();
+                if (userInput == "-send")
+                    SendGetRequest();
+
+                userInput = Console.ReadLine();
+            }
+        }
+
+        private static void SendGetRequest()
+        {
+            var client = new RestClient("https://tester.consimple.pro")
+            {
+                Timeout = -1
+            };
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            WriteResponse(response.Content);
+        }
+
+        private static void WriteResponse(string content)
+        {
+            Console.WriteLine("Result is: \n");
+            Data parsedJson = JsonSerializer.Deserialize<Data>(content);
+
+            ConsoleTable table = new();
+            table.AddColumn(new List<string>() { "Products", "Categories" });
+            foreach(var item in parsedJson.Products)
+            {
+                Categories category = parsedJson.Categories.Where(categ => categ.Id == item.CategoryId)
+                                                           .FirstOrDefault();
+                table.AddRow(item.Name, category.Name);
+            }
+            table.Write();
+        }
+
+        private static void WriteHelpInfoInConsole()
+        {
+            Console.WriteLine(@"Commands 
+                                    -help: Get more info
+                                    -send: Send http GET request to get Data
+                                    -exit: Exit and close programm
+            ");
+        }
+    }
+}
